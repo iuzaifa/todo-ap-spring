a36a2f97fbde5851dc0554954fcfb86f616ef55b
Abu Huzaifa <abuhuzaifaw7@gmail.com>
2026-05-24 01:09:30 +0530

    Spring Authentication added

---
+++ src/main/java/com/example/SpringAuthentication/SpringAuthenticationApplication.java
@@ -0,0 +1,14 @@
+package com.example.SpringAuthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAuthenticationApplication.class, args);
+		System.out.println("running............./");
+	}
+
+}
